0b201b8d6c64702e75f9fcfa1569d638e9727391
FANOR CHOQUE AGUIRRE <fanorarmandochoque@gmail.com>
2022-04-04 07:34:57 +0000

    solved webpack 5

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Fanor <code>Choque Aguirre</code> 2022.25sdfsd
+          Fanor <code>Choque Aguirre</code> 1945
         </p>sa
         <a
           className="App-link"
