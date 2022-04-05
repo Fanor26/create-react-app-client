50f98878a59831e336233a45a319a4fe7255b6cf
FANOR CHOQUE AGUIRRE <fanorarmandochoque@gmail.com>
2022-04-05 01:48:55 +0000

    setup material-ui

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Fanor <code>Choque Aguirre</code> 1945
+          Fanor <code>Choque Aguirre</code> 1987aasdsdsadsdsasdas
         </p>sa
         <a
           className="App-link"
